a2f50271726126cf3e835e00e4cbdb28e0060321
Joao M Dalla Vecchia <59539338+mattdvphx@users.noreply.github.com>
2020-05-03 13:32:50 -0700

    added questions on js

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const until = require("until");
+const writeFileAsync = util.promisify(fs.writeFile); 
 
-];
+function promptUser() {
+     return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your GitHub id?"
+      },
+      {
+        type: "input",
+        name: "repository",
+        message: "give a name to your repository?"
+      },
+      {
+        type: "input",
+        name: "details",
+        message: "write some details for the project"
+      },
+      {
+        type: "input",
+        name: "user interface",
+        message: "how will this project help the user?"
+      }
+      
+    ]);
+  }
 
 function writeToFile(fileName, data) {
 }
