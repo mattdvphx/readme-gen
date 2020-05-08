26798a1fae4357d24501b6a0319ddac032d49cd9
Joao M Dalla Vecchia <59539338+mattdvphx@users.noreply.github.com>
2020-05-08 14:17:32 -0700

    added functions

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,7 +1,44 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+# ${data.projectTitle}
 
+
+## table of contents
+ * Project title
+ * Description
+ * Installation
+ * Usage
+ * License
+ * Contributing
+ * Tests
+ * Questions
+
+## project description 
+${data.description}
+
+## installation
+${data.installation}
+
+## usage
+${data.usage}
+
+## license
+${data.license}
+
+## contributors
+${data.contributors}
+
+## tests
+${data.tests}
+
+## questions
+${data.questions}
+__Questions__: ask them at StackOverflow with the tag *REPO*.<br>
+[![StackOverflow](http://img.shields.io/badge/stackoverflow-REPO-blue.svg)]( http://stackoverflow.com/questions/tagged/REPO )
+
+![Github Picture](${data.avatar_url})
+
+${data.email}
 `;
 }
 
--- index.js
+++ index.js
@@ -1,39 +1,102 @@
 const inquirer = require("inquirer");
+const axios = require("axios");
 const fs = require("fs");
-const until = require("until");
-const writeFileAsync = util.promisify(fs.writeFile); 
+const generateMarkdown = require("./generateMarkdown.js");
 
-function promptUser() {
-     return inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your GitHub id?"
-      },
-      {
-        type: "input",
-        name: "repository",
-        message: "give a name to your repository?"
-      },
-      {
-        type: "input",
-        name: "details",
-        message: "write some details for the project"
-      },
-      {
-        type: "input",
-        name: "user interface",
-        message: "how will this project help the user?"
-      }
-      
-    ]);
+
+// * At least one badge
+// * Project title
+// * Description
+// * Table of Contents
+// * Installation
+// * Usage
+// * License
+// * Contributing
+// * Tests
+// * Questions
+
+const promptQuestions = [
+  {
+    type: "input",
+    name: "gitHubId",
+    message: "What is your GitHub id?"
+  },
+  {
+    type: "input",
+    name: "projectTitle",
+    message: "give a name to your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "write description for the project"
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "how will you install it?"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "what will this project be used for?"
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "what type of license for this project?",
+    choices:[
+      "MIT",
+      "Apache_2.0",
+      "Gpl_3.0",
+      "Bsd3",
+      "Other"
+    ]
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "who are the contributors for this project?"
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "tests?"
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: "questions?"
+  }
+]
+
+function promptUser(){
+  return inquirer.prompt(promptQuestions);
+}
+
+async function startProgram(){
+  const data = await promptUser(); 
+  const queryUrl = (githubName) => `https://api.github.com/users/${githubName}`;
+
+  const githubapi = {
+    
+    gitProfilePic: async function(githubName) {
+    const url = queryUrl(githubName);
+    const {data: {avatar_url}} = await axios.get(url);
+    return avatar_url;
+},
+gitEmail: async function(githubName) {
+    const url = `https://api.github.com/users/${githubName}/events/public`;
+    const res = await axios.get(url);
+    return res.data[0].payload.commits[0].author.email;
+}
+
   }
 
-function writeToFile(fileName, data) {
+  data.avatar_url = await githubapi.gitProfilePic(data.githubName);
+  data.email = await githubapi.gitEmail(data.githubName);
+  const readmemarkdown = generateMarkdown(data);
+  fs.writeFile("newReadme.md", readmemarkdown);
 }
 
-function init() {
-
-}
-
-init();
+startProgram();
